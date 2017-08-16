bf6e66808df5059c0dd412798b26b2f989416e93
EMENGH <eamendez62@gmail.com>
2017-08-16 09:57:21 -0400

    modified styling to home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -23,7 +23,7 @@ class Home extends Component {
 
   render() {
 
-    const TitleDiv = styled.div`
+    const HomePage = styled.div`
     display: flex;
     flex-direction: column;
     height: 95vh;
@@ -31,9 +31,8 @@ class Home extends Component {
     align-items: center;
     color: red;
     background-image: url("http://i.imgur.com/Q4fVZvt.jpg");
-    background-repeat: no-repeat;
-    background-attachment: fixed;
-    background-position: center;
+    background-size: cover;
+
     h1{
         color: white;
         text-align: center;
@@ -43,11 +42,11 @@ class Home extends Component {
     
     p{
         color: white;
-        font-size: 35px;
+        font-size: 30px;
         border-radius: 20px;
         text-decoration: none;
         background-color: red;
-        opacity: 0.2;
+        opacity: 0.8;
         padding: 20px 30px;
     }
 
@@ -58,30 +57,20 @@ class Home extends Component {
         border-radius: 20px;
         text-decoration: none;
         background-color: red;
-        opacity: 0.7;
+        opacity: 0.8;
         padding: 20px 30px;
     }
 `
     return (
-        <TitleDiv>
+        <HomePage>
       <div>
-        {/* <div>
-          <h1>What is your username?</h1>
-          <input type="text" name="user" />
-          <button>New User</button>
-        </div> */}
-        {/* {this.state.users.map((user, i) => (
-          <div key={i}>
-            <Link to={`/user/${user._id}`}>
-              {user.user}'s User
-            </Link>
-          </div>
-        ))} */}
+
         <h1>WELCOME TO RECIPE DEPOT</h1>
-        <p>The main purpose of this application is to offer a channel to explore, discover, and contribute to create a great collection of recipes. This repository is organized by cuisine types like chinese, italian, etc, to facilitate finding a particular recipe. You can also have the possibility to look at recipe reviews or add reviews to a specific recipe. This may give you a handy tool to choose from popular concoctions and perhaps it may encourage you to try some of these popular dishes..</p>
-        <Link to="/selectcuisine">Next</Link>
+        <p>The main purpose of this application is to offer a channel to explore, discover, and contribute to create a great collection of recipes. This repository is organized by cuisine types like chinese, italian, etc, to facilitate finding a particular recipe. It also offers the possibility to look at recipe reviews or add reviews to a specific recipe. This may give you a handy tool to choose from popular concoctions and perhaps it may encourage you to try some of these popular dishes..</p>
+        
+        <Link to="/selectcuisine">Begin</Link>
       </div>
-      </TitleDiv>
+      </HomePage>
     );
   }
 }
